30f4965e94ba2c0bd4f274ba5ef890a2db5abbcf
Jose Alberto Solis <josealbertosolis@MacBook-Pro-de-Jose.local>
2021-04-28 18:35:47 +0200

    New js for news content and NewsCard fixed

--- src/components/News.vue
+++ src/components/News.vue
@@ -2,32 +2,46 @@
   <div class="news">
     <div class="container">
       <h3>Noticias</h3>
-      <NewsCard />
+      <vueper-slides
+        class="no-shadow"
+        :visible-slides="4"
+        slide-multiple
+        :gap="3"
+        :slide-ratio="1 / 3"
+        :dragging-distance="200"
+        :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }"
+      >
+        <vueper-slide v-for="(article, index) in news" :key="index">
+          <template v-slot:content>
+            <NewsCard :article="article" />
+          </template>
+        </vueper-slide>
+      </vueper-slides>
     </div>
   </div>
 </template>
 
 <script setup>
-import NewsCard from './NewsCard.vue'
-
+import NewsCard from "./NewsCard.vue";
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
+import { news } from "../content/news.js";
 </script>
 
 <style scoped>
-.news{
-    height: 640px;
-    width: 100%;
-    background-color: var(--white-00);
+.news {
+  height: 640px;
+  width: 100%;
+  background-color: var(--white-00);
 }
-.container{
-    max-width: var(--app-max-width);
-    height: 100%;
-    margin: 0 auto;
-    padding: 2rem;
-
-    
+.container {
+  max-width: var(--app-max-width);
+  height: 100%;
+  margin: 0 auto;
+  padding: 2rem;
 }
-h3{
-    text-align: left;
-    font-size: var(--font-03);
+h3 {
+  text-align: left;
+  font-size: var(--font-03);
 }
 </style>
---
+++ src/content/news.js
@@ -0,0 +1,103 @@
+export const news = [
+    {
+        img:
+            "https://marmoransoprano.es/wp-content/uploads/2021/01/A9_05039-scaled-300x200.jpg",
+        text:
+            "Necesario García Leoz - La crítica de Pablo Siena sobre Luna Clara",
+        source:
+            "",
+
+        url:
+            "https://pablosiana.wordpress.com/2021/02/03/necesario-jesus-garcia-leoz",
+    },
+    {
+        img:
+            "https://marmoransoprano.es/wp-content/uploads/2021/02/portada-DEFINITIVA-copia-scaled-300x200.jpg",
+        text:
+            "Luna Clara, un tributo a García Leoz - Entrevista en la dársena.",
+        source:
+            "https://marmoransoprano.es/wp-content/uploads/2021/02/radionacionalclasica-e1612390880165.png",
+        url:
+            "https://www.rtve.es/m/alacarta/audios/la-darsena/entrevista-mar-moran/5774376/?media=rne&fbclid=IwAR2WwiPsvd7CmmCA3O_glevOeLwRT0jchY3E4e1gXXTF3njrd67geJyzO7E",
+    },
+    {
+        img:
+            "https://marmoransoprano.es/wp-content/uploads/2020/11/Mar-Moran-recibe-el-premio-Lola-Rodriguez-300x200.jpg",
+        text:
+            "La soprano Mar Morán recibe el Premio Lola Rodríguez de Aragón",
+        source:
+            "https://marmoransoprano.es/wp-content/uploads/2020/11/tlm-e1606596824835.jpeg",
+        url:
+            "http://todalamusica.es/la-sopreno-mar-moran-recibe-el-premio-lola-rodriguez-de-aragon/",
+    },
+    {
+        img:
+            "https://marmoransoprano.es/wp-content/uploads/2017/11/IMG_9615-2-scaled-300x200.jpg",
+        text:
+            "LA SOPRANO MAR MORÁN, PREMIO LOLA RODRÍGUEZ DE ARAGÓN 2020, DE LA ESCUELA SUPERIOR DE CANTO DE MADRID",
+        source:
+            "https://marmoransoprano.es/wp-content/uploads/2020/07/LogoPlateaNuevoCompleto-e1595799158681.png",
+        url:
+            "https://www.plateamagazine.com/noticias/10173-la-soprano-mar-moran-premio-lola-rodriguez-de-aragon-2020-de-la-escuela-superior-de-canto-de-madrid",
+    },
+    {
+        img:
+            "https://marmoransoprano.es/wp-content/uploads/2017/11/IMG_9615-2-scaled-300x200.jpg",
+        text:
+            "LA SOPRANO MAR MORÁN, PREMIO LOLA RODRÍGUEZ DE ARAGÓN 2020, DE LA ESCUELA SUPERIOR DE CANTO DE MADRID",
+        source:
+            "https://marmoransoprano.es/wp-content/uploads/2020/07/LogoPlateaNuevoCompleto-e1595799158681.png",
+        url:
+            "https://www.plateamagazine.com/noticias/10173-la-soprano-mar-moran-premio-lola-rodriguez-de-aragon-2020-de-la-escuela-superior-de-canto-de-madrid",
+    },
+    {
+        img:
+            "https://marmoransoprano.es/wp-content/uploads/2017/11/IMG_9615-2-scaled-300x200.jpg",
+        text:
+            "LA SOPRANO MAR MORÁN, PREMIO LOLA RODRÍGUEZ DE ARAGÓN 2020, DE LA ESCUELA SUPERIOR DE CANTO DE MADRID",
+        source:
+            "https://marmoransoprano.es/wp-content/uploads/2020/07/LogoPlateaNuevoCompleto-e1595799158681.png",
+        url:
+            "https://www.plateamagazine.com/noticias/10173-la-soprano-mar-moran-premio-lola-rodriguez-de-aragon-2020-de-la-escuela-superior-de-canto-de-madrid",
+    },
+    {
+        img:
+            "https://marmoransoprano.es/wp-content/uploads/2017/11/IMG_9615-2-scaled-300x200.jpg",
+        text:
+            "LA SOPRANO MAR MORÁN, PREMIO LOLA RODRÍGUEZ DE ARAGÓN 2020, DE LA ESCUELA SUPERIOR DE CANTO DE MADRID",
+        source:
+            "https://marmoransoprano.es/wp-content/uploads/2020/07/LogoPlateaNuevoCompleto-e1595799158681.png",
+        url:
+            "https://www.plateamagazine.com/noticias/10173-la-soprano-mar-moran-premio-lola-rodriguez-de-aragon-2020-de-la-escuela-superior-de-canto-de-madrid",
+    },
+    {
+        img:
+            "https://marmoransoprano.es/wp-content/uploads/2017/11/IMG_9615-2-scaled-300x200.jpg",
+        text:
+            "LA SOPRANO MAR MORÁN, PREMIO LOLA RODRÍGUEZ DE ARAGÓN 2020, DE LA ESCUELA SUPERIOR DE CANTO DE MADRID",
+        source:
+            "https://marmoransoprano.es/wp-content/uploads/2020/07/LogoPlateaNuevoCompleto-e1595799158681.png",
+        url:
+            "https://www.plateamagazine.com/noticias/10173-la-soprano-mar-moran-premio-lola-rodriguez-de-aragon-2020-de-la-escuela-superior-de-canto-de-madrid",
+    },
+    {
+        img:
+            "https://marmoransoprano.es/wp-content/uploads/2017/11/IMG_9615-2-scaled-300x200.jpg",
+        text:
+            "LA SOPRANO MAR MORÁN, PREMIO LOLA RODRÍGUEZ DE ARAGÓN 2020, DE LA ESCUELA SUPERIOR DE CANTO DE MADRID",
+        source:
+            "https://marmoransoprano.es/wp-content/uploads/2020/07/LogoPlateaNuevoCompleto-e1595799158681.png",
+        url:
+            "https://www.plateamagazine.com/noticias/10173-la-soprano-mar-moran-premio-lola-rodriguez-de-aragon-2020-de-la-escuela-superior-de-canto-de-madrid",
+    },
+    {
+        img:
+            "https://marmoransoprano.es/wp-content/uploads/2017/11/IMG_9615-2-scaled-300x200.jpg",
+        text:
+            "LA SOPRANO MAR MORÁN, PREMIO LOLA RODRÍGUEZ DE ARAGÓN 2020, DE LA ESCUELA SUPERIOR DE CANTO DE MADRID",
+        source:
+            "https://marmoransoprano.es/wp-content/uploads/2020/07/LogoPlateaNuevoCompleto-e1595799158681.png",
+        url:
+            "https://www.plateamagazine.com/noticias/10173-la-soprano-mar-moran-premio-lola-rodriguez-de-aragon-2020-de-la-escuela-superior-de-canto-de-madrid",
+    },
+]
