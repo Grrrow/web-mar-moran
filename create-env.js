7a8a18c839ff4f9f8f720ee719df282a7f125d3a
Borja Godoy <borja.godoy@altiplaconsulting.com>
2021-05-20 03:31:03 +0200

    add create-env

--- create-env.js
+++ create-env.js
@@ -1,2 +1,4 @@
 const fs = require('fs')
 fs.writeFileSync('./.env', `VITE_APP_EXAMPLE=${process.env.VITE_APP_EXAMPLE}\n`)
+console.log(process.env.VITE_APP_EXAMPLE)
+console.log(process.env.VUE_APP_EXAMPLE)
