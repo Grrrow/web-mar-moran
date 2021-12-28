f36fc5fa5b79d90fce8dc8a802328710d32d5431
borja.godoy <borja.godoy@ibm.com>
2021-12-28 19:41:00 +0100

    add biography translated

--- src/traductions/locale.js
+++ src/traductions/locale.js
@@ -15,44 +15,43 @@ export const messages = {
       knowMore: 'More',
       resume: {
         paragraph1:
-          '"La música y el ritmo encuentran su camino en los lugares secretos del alma" (Platon)',
+          '"Music and rhythm find their way into the secret places of the soul" (Platón).',
         paragraph2:
-          'Mar Morán estudió flauta pico en el Conservatorio de Badajoz y canto en la Escuela Superior de canto de Madrid.',
+        'Mar Morán began in music with her recorder studies at the Conservatory "Juan Vázquez" in Badajoz. She continued her studies in singing at the Escuela Superior de Canto de Madrid, completing her studies at the International Opera Academy in Ghent (Belgium).',
         paragraph3:
-          "Entre otros, estudió con Elena Muñoz Valdelomar, Victoria Manso, David Mason, Alicia Amo, Alex Ashworth, Sandra Medeiros, Armando Possante, Sylvia Schwartz, Stéphanie d'Oustrac, Anna Maria Ferrante, Boris Ignatov, Helen Tintes-Schuermann, Giulio Zappa, Charlotte Margiono, Dietrich Henschel y Plácido Domingo.",
+        "Her vocal training is consolidated with singing teacher Elena Muñoz Valdelomar, perfecting her technique with artists such as David Mason, Alicia Amo, Alex Ashworth, Sandra Medeiros, Armando Possante, Sylvia Schwartz, Stéphanie d'Oustrac, Anna Maria Ferrante, Nicola Beller Carbone, Martin Wölfel, Helen Tintes-Schuermann, Giulio Zappa, Ricardo Estrada, Charlotte Margiono, Dietrich Henschel and Plácido Domingo.",
         paragraph4:
-          'Mar ha recibido diferentes premios como el Premio Extraordinario y la Beca de Juventudes Musicales de la Comunidad de Madrid. Además, trabajó como soprano titular en el Coro del Teatro Real de Madrid durante varias temporadas.',
+        'Mar has been awarded with the Extraordinary Prize and the Scholarship of Juventudes Musicales de la Comunidad de Madrid in 2015, End of Career Award "Lola Rodriguez de Aragón" 2020, Primer Premi Absolut at the IV Concurs Internacional de Cant "Martín y Soler" 2021 in Polinyà del Xúquer (Valencia), 3rd Prize in the XXXVIII International Singing Contest "Ciudad de Logroño" 2021 in La Rioja in the category of female voices, 1st prize in the 9th International Contest of Habaneras for Lyric Soloists 2021 in Torrevieja, 1st prize in XXIV International Contest of Young performers Pedro Bote in Villafranca de lod Barros and 2nd prize in the IV Singing Contest Composetela Lirica 2021 in Santiago de Compostela.',
         paragraph5:
-          'En la actualidad está realizando un curso de postgrado en la Academia Internacional de Ópera de Gante (Bélgica).',
-      },
-      full: {
-        paragraph1:
-          'Nacida en Badajoz, Mar se graduó en el conservatorio de la ciudad, terminando el grado profesional de Flauta Pico. Durante estos años adquirió una sólida formación coral bajo la dirección de Alonso Gómez Gallego, siendo solista en el Coro del Conservatorio de Badajoz y en el Coro Amadeus de Puebla de la Calzada.',
-        paragraph2:
-          'Inició sus estudios vocales en el Conservatorio Profesional de Música "Victoria de los Ángeles" en Madrid y donde los finalizó obteniendo el Premio Fin de Carrera y el Premio Extraordinario de Música de la Comunidad de Madrid en la especialidad de Canto, bajo la tutela de su maestra de canto Elena Muñoz Valdelomar. Más tarde siguió en la Escuela Superior de Canto de Madrid junto a la maestra Victoria Manso.',
-        paragraph3:
-          'Mar recibió una beca de Juventudes Musicales en el año 2015 para estudiar en el extranjero otorgado de la mano de la Reina Sofía y también fue becada por la Asociación "Amigos de la Ópera de Madrid".',
-        paragraph4:
-          "Ha recibido clases magistrales de David Mason, Alicia Amo, Alex Ashworth, Delia Agúndez, Teresa Loring, José Hernández Pastor, Sandra Medeiros, Armando Possante, Sylvia Schwartz, Stéphanie d'Oustrac, Anna Maria Ferrante, Boris Ignatov, Helen Tintes-Schuermann, Nicola Beller Carbone, Giulio Zappa, Charlotte Margiono, Martin Wölfel, Dietrich Henschel y Plácido Domingo.",
-        paragraph5:
-          'Ha interpretado diferentes roles entre los que se destaca el rol "The Queen of the Night" en Die Zauberflöte de Mozart, Olympia en Les Contes d’Hoffmann de Offenbach y el de "Clarita" en la zarzuela La del Manojo de Rosas de Sorozábal realizado en varias ocasiones junto a la Sociedad Filarmónica de Badajoz y la Asociación A + Música.  Además ha participado en la VII Edición de la Ópera Joven de Jaén y como solista en varias galas en el Auditorio Nacional de Música de Madrid',
-        paragraph6:
-          "Formó parte en la sexta edición del taller para jóvenes cantantes líricos “Incanto Tignano” impartidos por la cantante Nicola Beller Carbone y en el 31º Festival Tignano per l'ambiente e l'incontro tra i popoli realizados en La Toscana, Florencia.",
-        paragraph7:
-          'Podemos destacar algunos recitales que ha ofrecido en la ciudad de Madrid como los realizados en La Fundación Juan March o en el Auditorio Nacional. En su tierra colaboró para el Instituto Extremeño de Canto y Dirección Coral de la Diputación de Badajoz en varias ocasiones. Formó parte de los carteles de los Festivales Internacionales de Música realizados en Gaia (Portugal) y Mendigorría (Navarra).',
-        paragraph8:
-          'De igual modo ha ofrecido recitales en directo para Radio Clásica (RNE, “Sinfonía de la mañana”) inaugurando la sección “Estudio 206”. Ha intervenido como solista ante S.M. Felipe VI en la X edición del Premio Europeo Carlos V. Ha participado también como solista en el concierto que el Coro Amadeus de Puebla de la Calzada ofreció en el Real Monasterio de Yuste en la conmemoración del “25 aniversario de la Fundación Academia Europea de Yuste”.',
-        paragraph9:
-          'Durante las temporadas 2016-2017, 2017-2018 y 2018-2019 ha formado parte del Coro del Teatro Real de Madrid como Soprano Titular participando en diversas temporadas.Es en esta etapa donde realiza un “partichino” en la ópera “Idomeneo” de W.A. Mozart sobre el escenario del Teatro Real (2019).',
-        paragraph10:
-          'Mar Morán fue becada por el “International Opera Academy” (IOA) de Gante en Bélgica para participar en el programa Opera Studio para jóvenes cantantes 19-20.',
-        paragraph11:
-          'A finales del año 2020 debutó con el rol de Kate Pinkerton en la producción de "Madama Butterfly" llevada a cabo en el Teatro Campoamor de Oviedo y compartiendo cartel con artistas como la soprano Ainhoa Arteta y el tenor Jorge de León. Además en el mes de noviembre del mismo año se convirtió en la ganadora del Premio Fin de Carrera Lola Rodríguez de Aragón 2020 concedido por la Escuela Superior de Canto de Madrid.',
-        paragraph12:
-          'Recientemente lanzará su primer disco “Luna clara” junto al pianista Aurelio Viribay respaldado por el se llo discográfico norteamericano Odradek Records, para grabar la integral de canciones del compositor español Jesús García Leoz que será distribuido entre otros lugares, en República Checa, España, Portugal, Suecia, Reino Unido, Estados Unidos, Canadá... así como en las principales plataformas de difusión musical (Blackberry BBM Music, Deezer, Eircom, IMVU, JB Hi Fi, Lala, Last.fm, MediaNet, Myspace, Nokia, Omnifone, Rara, Rdio, Rhpsody, SiriusXM Musicreports, SonyEricsson, Sony Qriosity, Spotify, Thumbplay, Turntable Fm, Youtube, Zike, Zune...',
-        paragraph13:
-          'Actualmente pertenece a la agencia internacional de artistas “Ascolta Artist” de mano del agente Rubén Martínez.',
-      },
+        'She has recently released her first album "Luna clara" with pianist Aurelio Viribay, backed by the American record label Odradek Records, which collects the integral songs of the Spanish composer Jesús García Leoz. She will soon debut in the role of "Queen of the Night" from Mozart\'s opera "The Magic Flute" in the 21/22 season of the Campoamor Theater in Oviedo.',      },
+        full: {
+          paragraph1:
+            'Born in Badajoz, Mar graduated from the conservatory of the city, finishing the professional degree of Pico Flute. During these years she acquired a solid choral training under the direction of Alonso Gómez Gallego, being a soloist in the Choir of the Conservatory of Badajoz and in the Amadeus Choir of Puebla de la Calzada.',
+          paragraph2:
+            'She began her vocal studies at the Professional Conservatory of Music "Victoria de los Ángeles" in Madrid and where she finished them obtaining the End of Career Prize and the Extraordinary Prize of Music of the Community of Madrid in the specialty of Singing, under the tutelage of her singing teacher Elena Muñoz Valdelomar. Later she continued at the Escuela Superior de Canto de Madrid with the teacher Victoria Manso.',
+          paragraph3:
+            'Mar received a scholarship from Juventudes Musicales in 2015 to study abroad granted by the hand of Queen Sofia and was also awarded a scholarship by the Association "Amigos de la Ópera de Madrid".',
+          paragraph4:
+            "She has received master classes from David Mason, Alicia Amo, Alex Ashworth, Delia Agúndez, Teresa Loring, José Hernández Pastor, Sandra Medeiros, Armando Possante, Sylvia Schwartz, Stéphanie d'Oustrac, Anna Maria Ferrante, Boris Ignatov, Helen Tintes-Schuermann, Nicola Beller Carbone, Giulio Zappa, Charlotte Margiono, Martin Wölfel, Dietrich Henschel, and Plácido Domingo.",
+          paragraph5:
+            'She has interpreted different roles among which the role "The Queen of the Night" in Mozart\'s Die Zauberflöte, Olympia in Offenbach\'s Les Contes d\'Hoffmann and "Clarita" in Sorozábal\'s zarzuela La del Manojo de Rosas performed on several occasions with the Sociedad Filarmónica de Badajoz and the Asociación A + Música.  She has also participated in the VII Edition of the Ópera Joven de Jaén and as a soloist in several galas at the Auditorio Nacional de Música de Madrid',
+          paragraph6:
+           'She took part in the sixth edition of the workshop for young lyric singers "Incanto Tignano" given by the singer Nicola Beller Carbone and in the 31st Tignano Festival per l\'ambiente e l\'incontro tra i popoli held in La Toscana, Florence.',
+          paragraph7:
+            'We can highlight some recitals he has offered in the city of Madrid such as those performed at La Fundación Juan March or at the Auditorio Nacional. In his homeland he collaborated for the Instituto Extremeño de Canto y Dirección Coral de la Diputación de Badajoz on several occasions. He was part of the posters of the International Music Festivals held in Gaia (Portugal) and Mendigorría (Navarra).',
+          paragraph8:
+            'He has also given live recitals for Radio Clásica (RNE, "Sinfonía de la mañana") inaugurating the section "Estudio 206". He has intervened as soloist before H.M. Felipe VI in the X edition of the European Prize Carlos V. He has also participated as soloist in the concert that the Amadeus Choir of Puebla de la Calzada offered in the Royal Monastery of Yuste in commemoration of the "25th anniversary of the European Academy of Yuste Foundation".',
+          paragraph9:
+            'During the 2016-2017, 2017-2018 and 2018-2019 seasons she has been part of the Choir of the Royal Theatre of Madrid as Titular Soprano participating in several seasons.It is in this stage where she performs a "partichino" in the opera "Idomeneo" by W.A. Mozart on the stage of the Royal Theatre (2019).',
+          paragraph10:
+            'Mar Morán was awarded a scholarship by the "International Opera Academy" (IOA) of Ghent in Belgium to participate in the Opera Studio program for young singers 19-20.',
+          paragraph11:
+            'At the end of 2020 she debuted with the role of Kate Pinkerton in the production of "Madama Butterfly" held at the Teatro Campoamor in Oviedo and sharing the bill with artists such as soprano Ainhoa Arteta and tenor Jorge de León. In addition, in November of the same year she became the winner of the "Premio Fin de Carrera Lola Rodríguez de Aragón 2020" awarded by the Escuela Superior de Canto de Madrid.',
+          paragraph12:
+            'She will recently release her first album "Luna clara" together with pianist Aurelio Viribay backed by the North American record label Odradek Records, to record the integral of songs by Spanish composer Jesús García Leoz which will be distributed among other places, in Czech Republic, Spain, Portugal, Sweden, United Kingdom, United States, Canada. ... as well as in the main platforms of musical diffusion (Blackberry BBM Music, Deezer, Eircom, IMVU, JB Hi Fi, Lala, Last.fm, MediaNet, Myspace, Nokia, Omnifone, Rara, Rdio, Rhpsody, SiriusXM Musicreports, SonyEricsson, Sony Qriosity, Spotify, Thumbplay, Turntable Fm, Youtube, Zike, Zune...',
+          paragraph13:
+            'Currently belongs to the international artist agency "Ascolta Artist" by agent Ruben Martinez.'
+        },
     },
     news: {
       title: 'News',
@@ -105,7 +104,7 @@ export const messages = {
         paragraph3:
           "Su formación vocal se consolida con la maestra de canto Elena Muñoz Valdelomar, perfecionando su técnica con artistas como David Mason, Alicia Amo, Alex Ashworth, Sandra Medeiros, Armando Possante, Sylvia Schwartz, Stéphanie d'Oustrac, Anna Maria Ferrante, Nicola Beller Carbone, Martin Wölfel, Helen Tintes-Schuermann, Giulio Zappa, Ricardo Estrada, Charlotte Margiono, Dietrich Henschel y Plácido Domingo.",
         paragraph4:
-          'Mar ha sido premiada con el Premio Extraordinario y la Beca de Juventudes Musicales de la Comunidad de Madrid en el año 2015, Premio fin de carrera "Lola Rodriguez de Aragón" 2020, Primer Premi Absolut en el IV Concurs Internacional de Cant "Martín y Soler" 2021 en Polinyà del Xúquer (Valencia), 3º Premio en el XXXVIII Concurso Internacional de Canto "Ciudad de Logroño" 2021 en La Rioja en la categoría de voces femeninas y 1º premio en el 9º Certamen Internacional de Habaneras para Solistas Líricos 2021 en Torrevieja.',
+          'Mar ha sido premiada con el Premio Extraordinario y la Beca de Juventudes Musicales de la Comunidad de Madrid en el año 2015, Premio fin de carrera "Lola Rodriguez de Aragón" 2020, Primer Premi Absolut en el IV Concurs Internacional de Cant "Martín y Soler" 2021 en Polinyà del Xúquer (Valencia), 3º Premio en el XXXVIII Concurso Internacional de Canto "Ciudad de Logroño" 2021 en La Rioja en la categoría de voces femeninas, 1º premio en el 9º Certamen Internacional de Habaneras para Solistas Líricos 2021 en Torrevieja, 1º premio en XXIV Certamen Internacional de Jóvenes intérpretes Pedro Bote en Villafranca de lod Barros y el 2º premio en el IV Concurso de Canto Composetela Lirica 2021 en Santiago de compostela.',
         paragraph5:
           'Recientemente ha lanzado su primer disco “Luna clara” junto al pianista Aurelio Viribay respaldado por el sello discográfico norteamericano Odradek Records, que recogen la integral de canciones del compositor español Jesús García Leoz. Próximamente debutará el rol "Reina de la Noche" de la Ópera "La Flauta Mágica" de Mozart en la temporada 21/22 del Teatro Campoamor de Oviedo.',
       },
