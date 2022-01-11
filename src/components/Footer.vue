85239c4161d60fbecc8f9062427d67515bc4ae78
Jose Alberto Solis <josealbertosolis@MacBook-Pro-de-Jose.local>
2022-01-11 18:38:43 +0100

    add footer to web

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main">
+        <div>
+            <img src="https://39373757.servicio-online.net/wp-content/uploads/2017/11/sign-300x38-1.png" alt="Mar Morán">
+        </div>
+        <div>
+            <p>© Copyright 2021 All Rights Reserved</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+img{
+    width: 220px;
+}
+.main{
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color:white;
+}
+</style>
